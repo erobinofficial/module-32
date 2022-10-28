1f5b0f58991111f29c760a52dfdb898b8d92f8cf
erobinofficial <erobin8778@gmail.com>
2022-10-29 00:34:07 +0600

    module-32-2

--- es6-recap.js
+++ es6-recap.js
@@ -1,3 +1,14 @@
 const hubby = 'Omar sani';
 let wife = 'mousumi';
+wife = 'shabnur'
+
+const maxNumber = (array = []) => {
+    const max = Math.max(...array);
+    return max;
+}
+const result = maxNumber([34, 345, 654, 5623, 643, 567, 3465]);
+const square = x => x * x;
+
+console.log(result);
+console.log(square(result));
 console.log(wife);
