8cf738895657ccbae80e5f8dc3c024e703e832c2
erobinofficial <erobin8778@gmail.com>
2022-10-29 17:23:57 +0600

    destructuring

---
+++ destructuring.js
@@ -0,0 +1,35 @@
+const mach = { nam : 'Cheng Mach', id: 50, address : 'India', price : 4000};
+// long way--------
+// const address = mach.address;
+// const price = mach.price;
+// const id = mach.id;
+
+// shortcut way--------
+const {price, id, nam} = mach;
+console.log(nam);
+
+const company = { 
+    name : 'return0',
+    address : 'Banani',
+    ceo : {
+        name: 'Mr Bush',
+        id : 24,
+        email : 'ceo@return0.com'
+    },
+    obejctive: {
+        service : 'Web Development',
+        employee: 15,
+        framework: {
+            fName : 'React',
+            version : 3,
+        }
+    }
+}
+const service = company.obejctive.service;
+console.log(service);
+
+const { name, email} = company.ceo;
+console.log(`Ceo name is : ${name} and Email is: ${email}`);
+
+const { fName, version} = company.obejctive.framework;
+console.log(fName ,version);
