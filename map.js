13689747dcd624eedac09b2816e4a8e884c5aa3c
erobinofficial <erobin8778@gmail.com>
2022-10-31 01:34:39 +0600

    32.4 finished

--- map.js
+++ map.js
@@ -1,7 +1,16 @@
 const numbers = [234, 432, 435, 445, 543, 436];
-const result = [];
-for(const number of numbers){
-    const changedArray = number * 2;
-    result.push(changedArray);
-}
-console.log(result);
+// const resultOld = [];
+
+// const sumIt = number => number*2;
+
+// for(const number of numbers){
+//     const changedArray = sumIt(number);
+//     result.push(changedArray);
+// }
+
+// console.log(...resultOld);
+
+// const resultNew = numbers.map(sumIt);
+// const resultNew = numbers.map(number  => number*2);
+const resultNew = numbers.map(x => x * 2);
+console.log(resultNew);
