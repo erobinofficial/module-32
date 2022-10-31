7f796e2795924292e86f1d2d179213b5414b9c23
erobinofficial <erobin8778@gmail.com>
2022-10-31 20:00:25 +0600

    module-5

---
+++ more-map.js
@@ -0,0 +1,17 @@
+// const students = ['Munna', 'Razu', 'Nahid', 'Nabil', 'Jahid'];
+// const sLenght = students.map(x => x.length);
+// console.log(sLenght);
+const products = [
+    {name: 'Water pot', color: 'Grey', size: 'Medium', price: '200'},
+    {name: 'Mouse', color: 'Black', size: 'Big', price: '600'},
+    {name: 'Earbuds', color: 'Black', size: 'Small', price: '1200'},
+    {name: 'Metador Pen', color: 'transparent', size: 'regular', price: '10'},
+    {name: 'Scale', color: 'White', size: 'Free', price: '20'}
+]
+
+const productsNames = products.map(x => x.name);
+const productsPrices = products.map(x => x.price);
+console.log(productsNames);
+console.log(productsPrices);
+products.map(x => console.log(x.color));
+products.forEach(x => console.log(x.size));
