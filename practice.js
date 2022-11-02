ad94d1d2ead26af7655b5b0ba25297cfee93a9e0
erobinofficial <erobin8778@gmail.com>
2022-11-03 01:41:10 +0600

    practicing

---
+++ practice.js
@@ -0,0 +1,14 @@
+// const doMath = x => x*2;
+// console.log(doMath(5));
+
+// const doMath = (x, y) => {
+//     const add = x + 2;
+//     const add2 = y + 2;
+//     const multi = add * add2;
+//     return multi; 
+// };
+// console.log(doMath(2, 3));
+
+// const doMath = (x, y, z) => x * y * z;;
+// console.log(doMath(2, 5, 4));
+
