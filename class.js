ef182b69b03945ecb1af468c3066d5087750e5b4
erobinofficial <erobin8778@gmail.com>
2022-11-02 12:35:49 +0600

    Class finished

--- class.js
+++ class.js
@@ -1,5 +1,18 @@
 class Support {
     name;
-    
+    role;
+    address;
+    constructor(name, role, address){
+        this.name = name;
+        this.role = role;
+        this.address = address;
+    }
+    doWork(){
+        console.log(this.name, "Started to learning");
+    }
 }
-console.log('sao');
+const nahid = new Support('Nahid', 'Web Engineer', 'Sakhipur');
+const munna = new Support('Munna', 'Senior Manager', 'Ghatail');
+console.log(nahid);
+console.log(munna);
+nahid.doWork();
